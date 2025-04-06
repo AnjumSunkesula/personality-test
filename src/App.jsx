2afe968bee5d3d80c83d04eac363b5c62f60a15e
afroz <afrozdd7@gmail.com>
2025-04-06 14:39:26 +0530

    implemented routing for questionspage using react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
+import Welcome from './components/welcome';
+import QuestionPage from './components/QuestionPage';
+// import React,{ useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 
 function App() {
 
+  
+
   return (
     <>
+      <Router>
+        <Routes>
+          <Route exact path='/' Component={Welcome}/>
+          <Route path='/questions' Component={QuestionPage}/> 
+
+
+
+        </Routes>
+      </Router>
     
+
+        
     </>
   )
 }
