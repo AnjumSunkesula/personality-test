c4d31df0b04678345b79f12764905f63f3e83291
afroz <afrozdd7@gmail.com>
2025-04-06 16:48:37 +0530

    added 2 quiz questions

---
+++ src/data/questions.js
@@ -0,0 +1,23 @@
+const questions = [
+  {
+    question: "What's your ideal weekend?",
+    options: [
+      { text: "solo hiking", trait: "introvert",  },
+      { text: "Big group adventure 🚀", trait: "extrovert", emoji: "🚀" },
+      { text: "Chilling with 2-3 friends 🎮", trait: "ambivert", emoji: "🎮" },
+      { text: "Volunteering at an event 🤝", trait: "empathetic", emoji: "🤝" }
+    ],
+    category: "lifestyle"
+  },
+  {
+    question: "what's your ideal weekend?",
+    options: [
+      { text: "solo hiking", trait: "introvert",  },
+      { text: "Big group adventure 🚀", trait: "extrovert", emoji: "🚀" },
+      { text: "Chilling with 2-3 friends 🎮", trait: "ambivert", emoji: "🎮" },
+      { text: "Volunteering at an event 🤝", trait: "empathetic", emoji: "🤝" }
+    ],
+    category: "lifestyle"
+  }
+];
+export default questions;
