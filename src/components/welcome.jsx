07c664256e04b5e31c81b130ef976f1b179a807f
afroz <afrozdd7@gmail.com>
2025-04-08 16:13:41 +0530

    added fade-in-out animation for heading

--- src/components/welcome.jsx
+++ src/components/welcome.jsx
@@ -18,7 +18,7 @@ function Welcome() {
           animationDuration: "5s",
           animationTimingFunction: "ease-in-out"
         }}></div>
-        <div className=" text-center font-sans capitalize bg-gradient-to-r from-violet-800 to-pink-400 bg-clip-text text-transparent text-5xl py-2">discover your personality profile</div>
+        <div className=" text-center font-sans capitalize bg-gradient-to-r from-violet-800 to-pink-400 bg-clip-text text-transparent text-5xl py-2 animate-pulse">discover your personality profile</div>
         <div className=" bg-neutral-50 py-3 px-8 rounded-full text-purple-800 shadow-xl inset-shadow-sm capitalize font-medium transition duration-700 ease-in-out hover:-translate-y-1">
           <button className="flex items-center gap-2 capitalize" onClick={handleClick}>start assessment<span className="pt-0.5"><FontAwesomeIcon icon={faArrowRight} /></span></button>
         </div>
